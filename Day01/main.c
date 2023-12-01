ab8bd3c06a4631784cc28186d7be28804cfce48e
Vilja Kettunen <vkettune@c1r6p6.hive.fi>
2023-12-01 17:04:29 +0200

    edit day01

--- Day01/main.c
+++ Day01/main.c
@@ -6,24 +6,23 @@
 /*   By: vkettune <vkettune@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 12:53:34 by vkettune          #+#    #+#             */
-/*   Updated: 2023/12/01 15:03:36 by vkettune         ###   ########.fr       */
+/*   Updated: 2023/12/01 17:04:05 by vkettune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int combine(int first, int last)
+int	combine(int first, int last)
 {
-	int num = first * 10;
-	num += last;
+	int	num;
+
+	num = (first * 10) + last;
 	return (num);
 }
 
-int strlen_i(int i, char **argv)
+int	end(int i, int j, char **argv)
 {
-	int j = 0;
-
 	while (argv[i][j] != '\0')
 		j++;
 	return (j);
@@ -31,30 +30,30 @@ int strlen_i(int i, char **argv)
 
 int	*find_value(int argc, char *argv[])
 {
-	int i = 0;
-	int j = 0;
-	int first = -1;
-	int last = -1;
-	int sum = 0;
-	int *res;
+	int	i = 0;
+	int	j = 0;
+	int	first = -1;
+	int	last = -1;
+	int	sum = 0;
+	int	*res;
 
 	while (argv[i] != NULL && i < argc) // while there are lines to decode
 	{
 		j = 0;
 		while (first < 0 && argv[i][j] != '\0') // find first num
 		{
-			if (argv[i][j] >= '0' && argv[i][j] <= '9') // is digit?
+			if (argv[i][j] >= '0' && argv[i][j] <= '9')
 				first = argv[i][j] - '0';
 			else
 				j++;
 		}
-		j = strlen_i(i, argv) - 1;
+		j = end(i, j, argv) - 1;
 		while (last < 0 && j >= 0) // find last num
 		{
-			if (argv[i][j] >= '0' && argv[i][j] <= '9') // is digit?
+			if (argv[i][j] >= '0' && argv[i][j] <= '9')
 			{
 				last = argv[i][j] - '0';
-				break; // Exit the loop once the last digit is found
+				break ;
 			}
 			else
 				j--;
@@ -72,17 +71,17 @@ int	*find_value(int argc, char *argv[])
 }
 
 /*
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    int *sum = find_value(argc, argv);
-    if (sum != NULL)
-    {
-        printf("The sum of all calibration values is %d\n", *sum);
-        free(sum);
-    }
-    else
-    {
-        printf("Failed to allocate memory.\n");
-    }
-    return 0;
-}*/
+	int	*sum;
+
+	sum = find_value(argc, argv);
+	if (sum != NULL)
+	{
+		printf("The sum of all calibration values is %d\n", *sum);
+		free(sum);
+	}
+	else
+		printf("Failed to allocate memory.\n");
+	return (0);
+}8/
